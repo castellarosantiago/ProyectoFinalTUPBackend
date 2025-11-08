037e52723493ea1e574dd5671db6b2ad870f7870
Mateo Muscolino <muscolinomateo@gmail.com>
2025-11-08 09:56:38 -0300

    Sale Schema

--- src/models/Sales.ts
+++
@@ -1,1 +0,0 @@
-//schema para las ventas
--- src/types/sales.interface.ts
+++ src/types/sales.interface.ts
@@ -1,16 +1,17 @@
-import {Document, Types} from 'mongoose'; // document le inyecta propiedades como save(), remove() etc
+import { Document, Types } from "mongoose"; // document le inyecta propiedades como save(), remove() etc
 
 //Interface para el detalle de cada producto vendido
-export interface SaleDetail {
-    product: Types.ObjectId;
-    name:string;
-    amountSold:number;
-    subtotal: number; // precio *cantidad
+export interface ISaleDetail {
+  product: Types.ObjectId;
+  name: string;
+  amountSold: number;
+  subtotal: number; // precio *cantidad
 }
 
 //Interface principal para Venta
-export interface Sale extends Document {
-    date: Date;
-    user: Types.ObjectId; // hace referencia al usuario (Empleado/Admin) que hizo la venta para que quede el registro
-    detail: SaleDetail[] // array de productos vendidos
+export interface ISale extends Document {
+  date: Date;
+  user: Types.ObjectId; // hace referencia al usuario (Empleado/Admin) que hizo la venta para que quede el registro
+  detail: ISaleDetail[]; // array de productos vendidos
+  total: number;
 }
