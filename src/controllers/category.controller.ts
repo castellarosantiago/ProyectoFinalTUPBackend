94836016508685d37f1b577643ee63fbe439a641
Paula General <generalpaulawrk@gmail.com>
2025-11-11 16:28:43 -0300

    Category Controller modificado implementando el uso de middlewares

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import CategoryRepository from "../repositories/Category.repository";
 import { CategoryInputInterface } from "../types/categoryType";
+import { Types } from "mongoose";
 
 class CategoryController {
 
@@ -17,15 +18,10 @@ class CategoryController {
 
     public async findCategoryById(req:Request, res:Response){
         const id = req.params.id;
-    // MIDDLEWARES? JOI
-        if (!id){
-            return res.status(400).json({ message: "Debe ingresar un ID para buscar la Categoría." });
-        }
-
-        //TODO: validar si el id no es de tipo ObjectID 
 
         try {
-            const category = await CategoryRepository.findCategoryById(id); // tiene que pasarse como ObjectID
+            const objectId = new Types.ObjectId(id);
+            const category = await CategoryRepository.findCategoryById(objectId);
 
             if (!category){
                 return res.status(404).json({ message: "No se pudo encontrar una categoría con el ID ingresado."});
@@ -39,16 +35,12 @@ class CategoryController {
         }
     }
 
-    public createCategory(req:Request, res:Response){
+    public async createCategory(req:Request, res:Response){
         const { name, description } = req.body;
-    //MIDDLEWARES?? JOI
-        if (!name) {
-            return res.status(400).json({ message: "Debe ingresar el campo name para crear la categoría." });
-        }
         const dataValidate:CategoryInputInterface = { name, description}
 
         try {
-            const newCategory = CategoryRepository.createCategory(dataValidate);
+            const newCategory = await CategoryRepository.createCategory(dataValidate);
             return res.status(201).json({ message: "Categoría creada correctamente.", category:newCategory });
         } catch (err){
             if (err instanceof Error){
@@ -57,15 +49,12 @@ class CategoryController {
         }
     }    
 
-    public deleteCategory(req:Request, res:Response){
+    public async deleteCategory(req:Request, res:Response){
         const id = req.params.id;
-    //MIDDLEWARE
-        if (!id){
-            return res.status(400).json({ message: "Debe ingresar un ID para eliminar la categoría." });
-        }
 
         try {
-            const categoryDeleted = CategoryRepository.deleteCategory(id); // debe pasar como ObjectID
+            const objectId = new Types.ObjectId(id);
+            const categoryDeleted = await CategoryRepository.deleteCategory(objectId); 
 
             if (!categoryDeleted){
                 return res.status(404).json({ message: "No se pudo borrar la categoría con el ID ingresado."});
@@ -79,24 +68,14 @@ class CategoryController {
         }
     }
 
-
-    public updateCategory(req:Request, res:Response){
+    public async updateCategory(req:Request, res:Response){
         const id = req.params.id;
         const { name, description } = req.body;
-
-    // MIDDLEWARE
-        if (!id){
-            return res.status(400).json({ message: "Debe ingresar un ID para modificar la categoría" });
-        }
-
-        if (!name) {
-            return res.status(400).json({ message: "Debe ingresar el campo name para crear la categoría." });
-        }
-        
         const dataValidate:CategoryInputInterface = { name, description}
 
         try {
-            const updatedCategory = CategoryRepository.updateCategory(id, dataValidate); // debe pasar como ObjectID
+            const objectId = new Types.ObjectId(id);
+            const updatedCategory = await CategoryRepository.updateCategory(objectId, dataValidate);
             return res.status(200).json({ message: "Categoría modificada correctamente.", category:updatedCategory});
         } catch (err){
             if (err instanceof Error){
