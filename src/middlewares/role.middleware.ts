b5c879ae180f2b48b33a174906def1a243911081
Paula General <generalpaulawrk@gmail.com>
2025-11-26 20:57:32 -0300

    Middleware para verificar rol (Admin) y restringir rutas

--- src/middlewares/role.middleware.ts
+++ src/middlewares/role.middleware.ts
@@ -1,1 +1,20 @@
-//verifica el rol del usuario (admin o user)
+import { Request, Response, NextFunction } from "express";
+
+// verifica el rol del usuario (admin o empleado)
+export function authorizeAdmin(req:Request, res:Response, next:NextFunction) {
+    const role = req.user?.role; // verificar que en jwt se agregue user y crear interface
+
+    if (!role) {
+        return res.status(401).json({ error: "Usuario no autenticado" });
+    }
+
+    if (role !== "admin") {
+        return res.status(403).json({ error: "Acceso permitido solo para administradores" });
+    }
+
+    next();
+}
+
+
+
+
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,13 +3,14 @@ import categoryController from "../controllers/category.controller";
 import { categoryBodySchema } from "../schemas/category.schema";
 import { idSchema } from "../schemas/id.schema";
 import { validate } from "../middlewares/validator.middleware";
+import { authorizeAdmin } from "../middlewares/role.middleware";
 
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.getCategories);
 categoryRouter.get("/:id", validate(idSchema, "params") ,categoryController.findCategoryById);
-categoryRouter.delete("/:id", validate(idSchema, "params"), categoryController.deleteCategory);
-categoryRouter.post("/", validate(categoryBodySchema, "body"), categoryController.createCategory);
-categoryRouter.put("/:id", validate(idSchema, "params"), validate(categoryBodySchema, "body"), categoryController.updateCategory);
+categoryRouter.delete("/:id", authorizeAdmin ,validate(idSchema, "params"), categoryController.deleteCategory);
+categoryRouter.post("/", authorizeAdmin, validate(categoryBodySchema, "body"), categoryController.createCategory);
+categoryRouter.put("/:id", authorizeAdmin, validate(idSchema, "params"), validate(categoryBodySchema, "body"), categoryController.updateCategory);
 
 export default categoryRouter;
