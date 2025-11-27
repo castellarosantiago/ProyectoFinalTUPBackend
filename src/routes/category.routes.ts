567b806592f3e62246458f149b12aac3780ffc3b
Paula General <generalpaulawrk@gmail.com>
2025-11-26 23:05:04 -0300

    Implementacion de Middleware de Autenticacion en las rutas

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,13 +3,14 @@ import categoryController from "../controllers/category.controller";
 import { categoryBodySchema } from "../schemas/category.schema";
 import { idSchema } from "../schemas/id.schema";
 import { validate } from "../middlewares/validator.middleware";
+import authenticate from "../middlewares/auth.middleware";
 
 const categoryRouter = Router();
 
-categoryRouter.get("/", categoryController.getCategories);
-categoryRouter.get("/:id", validate(idSchema, "params") ,categoryController.findCategoryById);
-categoryRouter.delete("/:id", validate(idSchema, "params"), categoryController.deleteCategory);
-categoryRouter.post("/", validate(categoryBodySchema, "body"), categoryController.createCategory);
-categoryRouter.put("/:id", validate(idSchema, "params"), validate(categoryBodySchema, "body"), categoryController.updateCategory);
+categoryRouter.get("/", authenticate, categoryController.getCategories);
+categoryRouter.get("/:id", authenticate, validate(idSchema, "params") ,categoryController.findCategoryById);
+categoryRouter.delete("/:id", authenticate, validate(idSchema, "params"), categoryController.deleteCategory);
+categoryRouter.post("/", authenticate, validate(categoryBodySchema, "body"), categoryController.createCategory);
+categoryRouter.put("/:id", authenticate, validate(idSchema, "params"), validate(categoryBodySchema, "body"), categoryController.updateCategory);
 
 export default categoryRouter;
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,18 +3,19 @@ import productController from "../controllers/product.controller";
 import { productBodyCreateSchema, productBodyPutSchema, productFilterPriceSchema, productSearchNameSchema, productFilterIdCategorySchema } from "../schemas/product.schema";
 import { idSchema } from "../schemas/id.schema";
 import { validate } from "../middlewares/validator.middleware";
+import authenticate from "../middlewares/auth.middleware";
 
 const productRouter = Router();
 
-productRouter.get("/", productController.getProducts);
-productRouter.get("/:id", validate(idSchema, "params") ,productController.findProductById);
-productRouter.delete("/:id", validate(idSchema, "params"), productController.deleteProduct);
-productRouter.post("/", validate(productBodyCreateSchema, "body"), productController.createProduct);
-productRouter.put("/:id", validate(idSchema, "params"), validate(productBodyPutSchema, "body"), productController.updateProduct);
+productRouter.get("/", authenticate, productController.getProducts);
+productRouter.get("/:id", authenticate, validate(idSchema, "params") ,productController.findProductById);
+productRouter.delete("/:id", authenticate, validate(idSchema, "params"), productController.deleteProduct);
+productRouter.post("/", authenticate, validate(productBodyCreateSchema, "body"), productController.createProduct);
+productRouter.put("/:id", authenticate, validate(idSchema, "params"), validate(productBodyPutSchema, "body"), productController.updateProduct);
 // Filtrado
-productRouter.get('/search/name', validate(productSearchNameSchema, "query"), productController.findProductByName);
-productRouter.get('/filter/category', validate(productFilterIdCategorySchema, "query"), productController.filterByCategory);
-productRouter.get('/filter/price', validate(productFilterPriceSchema, "query"), productController.filterByPrice);
+productRouter.get('/search/name', authenticate, validate(productSearchNameSchema, "query"), productController.findProductByName);
+productRouter.get('/filter/category', authenticate, validate(productFilterIdCategorySchema, "query"), productController.filterByCategory);
+productRouter.get('/filter/price', authenticate, validate(productFilterPriceSchema, "query"), productController.filterByPrice);
 
 
 export default productRouter;
