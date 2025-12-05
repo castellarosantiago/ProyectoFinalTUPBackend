10aaa2bb93999b19c4321c0b6fef793a509ddd2f
Santiago Castellaro <101475544+castellarosantiago@users.noreply.github.com>
2025-12-05 01:01:03 -0300

    sv fix para deploy

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const app = express();
 
 // middlewares globales
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
